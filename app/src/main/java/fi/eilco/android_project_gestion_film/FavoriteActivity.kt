8ca2da6ca56f952826b40ef72359b69bf16eeb80
Inebur <mahugnonbruneiadagbe@gmail.com>
2023-01-29 01:40:01 +0100

    favorite activity added

---
+++ app/src/main/java/fi/eilco/android_project_gestion_film/FavoriteActivity.kt
@@ -0,0 +1,11 @@
+package fi.eilco.android_project_gestion_film
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FavoriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite)
+    }
+}
